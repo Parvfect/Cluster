3a55f62d011318ced69862ae3007c9184bf92b64
Parvfect <parvagrw02@gmail.com>
2024-02-05 13:08:43 +0000

    here we go boys

--- coupon_collector.py
+++ coupon_collector.py
@@ -194,7 +194,7 @@ def get_parameters_sc_ldpc(n_motifs, n_picks, L, M, dv, dc, k, n, ffdim, display
     return Harr, H, G, graph, C, symbols, motifs, k, n
 
 
-def decoding_errors_fer(k, n, dv, dc, graph, C, symbols, motifs, n_picks, read_lengths = np.arange(1,12), decoding_failures_parameter=5, max_iterations=20, iterations=50, uncoded=False, bec_decode=False, label=None, code_class=""):
+def decoding_errors_fer(k, n, dv, dc, graph, C, symbols, motifs, n_picks, read_lengths = np.arange(1,12), decoding_failures_parameter=2, max_iterations=2000, iterations=50, uncoded=False, bec_decode=False, label=None, code_class=""):
     """ Returns the frame error rate curve - for same H, same G, same C"""
 
     frame_error_rate = []
@@ -211,8 +211,8 @@ def decoding_errors_fer(k, n, dv, dc, graph, C, symbols, motifs, n_picks, read_l
                 if bec_decode:
                     decoded_values = graph.coupon_collector_erasure_decoder()
                 else:
-                    #decoded_values = graph.coupon_collector_decoding()
-                    decoded_values = graph.adaptive_coupon_collector_decoding()
+                    decoded_values = graph.coupon_collector_decoding()
+                    #decoded_values = graph.adaptive_coupon_collector_decoding()
             else:
                 decoded_values = symbols_read
             
@@ -317,9 +317,9 @@ if __name__ == "__main__":
         n_motifs, n_picks = 8, 4
         dv, dc, ffdim = 3, 9, 67
         k, n = 500 ,1500
-        L, M = 30, 101
+        L, M = 50, 1001
         read_length = 6
-        read_lengths = np.arange(6,12)
+        read_lengths = np.arange(6,7)
         run_fer(n_motifs, n_picks, dv, dc, k, n, L, M, ffdim, code_class="sc_", read_lengths=read_lengths, saved_code=False, save_file=False)
     
 
